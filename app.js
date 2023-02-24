f9b6774f4192bd6988fa6e6280c34c5f68286a4c
kakeru-private <rkeka4855@gmail.com>
2023-02-24 10:25:36 +0900

    path change

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/users', usersRouter); 
+app.use('/', usersRouter); 
 app.use('/record', record); 
 app.use('/record/track', track); 
 app.use('/select', select); 
